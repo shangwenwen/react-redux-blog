9cb0b36124a8ce187414774b1ab57d4e0c0c528f
shangwenwen <1967553097@qq.com>
2018-04-01 13:47:50 +0800

    all

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,17 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: ['./src/index.js'],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
+  entry: {
+    app: './src/index.js',
+    vendor: ['react', 'react-dom']
   },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    chunkFilename: "[name].js"
+  },
+
   module: {
     rules: [
       {
@@ -18,74 +24,20 @@ module.exports = {
         exclude: /node_modules|packages/,
         test: /\.js$/,
         use: 'babel-loader',
-      },
+      }
     ],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       title: 'react-redux-blog',
+      template: './src/index.html',
+      hash: true,
       minify: {
         removeAttributeQuotes: true
-      },
-      hash: true,
-      template: './src/index.html'
+      }
     }),
-    new webpack.NamedModulesPlugin()],
-}
+    new webpack.NamedModulesPlugin()
+  ],
 
-// // webpack.config.js
-// const path = require('path')
-// const webpack = require('webpack')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
-//
-// module.exports = {
-//   entry: {
-//     app: './src/index.js',
-//     vendor: ['react', 'react-dom']
-//   },
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: "[name].js",
-//     publicPath: './',
-//     chunkFilename: "[name].js"
-//   },
-//   module: {
-//     rules: [
-//       {
-//         exclude: /node_modules|packages/,
-//         test: /\.js$/,
-//         use: 'babel-loader',
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       title: 'react-redux-blog',
-//       minify: {
-//         removeAttributeQuotes: true
-//       },
-//       hash: true,
-//       template: './src/index.html'
-//     }),
-//   ],
-//   optimization: {
-//     splitChunks: {
-//       cacheGroups: {
-//         commons: {
-//           chunks: 'initial',
-//           minChunks: 2,
-//           maxInitialRequests: 5,
-//           minSize: 0
-//         },
-//         vendor: {
-//           test: /node_modules/,
-//           chunks: 'initial',
-//           name: 'vendor',
-//           priority: 10,
-//           enforce: true
-//         }
-//       }
-//     },
-//     runtimeChunk: false
-//   }
-// }
+}
